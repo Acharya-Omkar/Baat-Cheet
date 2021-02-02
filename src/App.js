a2f5ddc5b2fffa4d68f3be5b8481c03ff7a24bb1
Omkar Acharya <49238582+Acharya-Omkar@users.noreply.github.com>
2021-02-02 14:21:23 +0530

    Chat app final

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+
+
+const projectID="ce21f49e-6214-4212-abef-0ec1e0089e0b";
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return <ChatEngine height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}/>;
+};
+
+export default App;
